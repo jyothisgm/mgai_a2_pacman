6817cda4d5ad5777bf44519f17eb0e2c2e39a529
GoelPri-uni <p.goel.2@umail.leidenuniv.nl>
2025-03-25 22:50:29 +0100

    updated torunament with shuffled

--- tournament.py
+++ tournament.py
@@ -6,15 +6,15 @@ import sys
 import pandas as pd
 from capture import readCommand, runGames, save_score
 
+import random
 
-
-def get_agent_files(folder_path,  leaderboard_f, match_history_f, num_games):
+def get_agent_files(folder_path,  leaderboard_f, match_history_f):
     agents = []
     for file in os.listdir(folder_path):
         if file.endswith(".py"):
-            agents.append(os.path.splitext(file)[0]) 
-    
-    all_elo_scores = choose_combination_(agents, num_games,  leaderboard_f, match_history_f)
+            agents.append(folder_path+"/"+file) 
+    print(agents)
+    all_elo_scores = choose_combination_(agents, leaderboard_f, match_history_f)
     return all_elo_scores
 
 def update_elo(red_agent, blue_agent, elo_scores, red_score, blue_score, K=32):
@@ -45,30 +45,45 @@ def get_elo_score(red, blue, elo_scores, scores_capture):
         update_elo(red, blue, elo_scores, red_score, blue_score)
     
     
-def choose_combination_(agents, num_games,  leaderboard_file, match_history_file):
+def choose_combination_(agents,   leaderboard_file, match_history_file):
     
     elo_scores = {agent: 1500 for agent in agents}
-    matchups = list(combinations(agents, 2))
-    match_history = []
-    match_count =1
-    max_elo = 1500
-    
-    for red, blue in matchups:
+    for _ in range(20):
+        matchups = list(combinations(agents, 2))
+        random.shuffle(matchups)
         
-        scores = run_match(red, blue, num_games)
-        get_elo_score(red, blue, elo_scores, scores)
+        match_history = []
+        match_count =1
+        max_elo = 1500
         
-        match_history.append({
-                "match_number": match_count,
-                "red_agent": red,
-                "blue_agent": blue,
+        for red, blue in matchups:
             
-                "elo_red_after": round(elo_scores[red], 2),
-                "elo_blue_after": round(elo_scores[blue], 2)
-            })
-        match_count +=1
         
-    
+            scores = run_match(red, blue, )
+            get_elo_score(red, blue, elo_scores, scores)
+            
+            match_history.append({
+                    "match_number": match_count,
+                    "red_agent": red,
+                    "blue_agent": blue,
+                
+                    "elo_red_after": round(elo_scores[red], 2),
+                    "elo_blue_after": round(elo_scores[blue], 2)
+                })
+            match_count +=1
+            
+            scores = run_match(blue, red)
+            get_elo_score(blue, red, elo_scores, scores)
+            
+            match_history.append({
+                    "match_number": match_count,
+                    "red_agent": blue,
+                    "blue_agent": red,
+                
+                    "elo_red_after": round(elo_scores[blue], 2),
+                    "elo_blue_after": round(elo_scores[red], 2)
+                })
+            match_count +=1
     df = pd.DataFrame(match_history)
     
     df.to_csv(match_history_file, index=False)
@@ -88,16 +103,16 @@ def choose_combination_(agents, num_games,  leaderboard_file, match_history_file
     elo_ranking_df.to_csv(leaderboard_file, index=False)
     print(f"Leaderboard saved to {leaderboard_file}")
 
-    return elo_scores
     
 
-def run_match(red_file, blue_file,  num_games=2):
+
+def run_match(red_file, blue_file):
     # Build command-line-like args
     args = [
         "-r", red_file,
         "-b", blue_file,
   
-        "--numGames", str(num_games),
+        # "--numGames", str(num_games),
         "-q"
       
     ]
@@ -116,20 +131,21 @@ def main():
         "--folder", type=str, default="tourna_files",
         help="Folder containing agent files"
     )
-    parser.add_argument(
-        "--games", type=int, default=20,
-        help="Number of games per match"
-    )
+    # parser.add_argument(
+    #     "--games", type=int, default=2,
+    #     help="Number of games per match"
+    # )
     args = parser.parse_args()
 
     # Construct agent folder path
     base_dir = os.path.dirname(__file__)
     agents_dir = os.path.join(base_dir, args.folder)
+  
     leaderboard_file = "elo_leaderboard.csv"
     match_history_file = "elo_match_history.csv"
 
     # Run tournament
-    agent_tournament = get_agent_files(agents_dir, leaderboard_file, match_history_file,num_games=args.games)
+    agent_tournament = get_agent_files(agents_dir, leaderboard_file, match_history_file)
      
 if __name__ == "__main__":
     main()
